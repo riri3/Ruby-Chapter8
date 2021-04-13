480b6c4b38cd55b859f5408294bce8aaaf842916
EC2 Default User <ec2-user@ip-172-31-84-219.ec2.internal>
2021-04-13 14:48:30 +0000

    [finish]2-7-2

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -3,6 +3,26 @@ puts "何回繰り返しますか？"
 
 x=gets.to_i
 
-for x in 1..x do
+i=1
 
-puts "x.to_s回目の計算"
+while i <= x do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  
+  a=gets.to_i
+  b=gets.to_i
+  
+  puts "a=#{a}"
+  puts "b=#{b}"
+  
+  puts "計算結果を出力します"
+  
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+  
+  i += 1
+end
+
+puts "計算を終了します"
